3a4f6011703bef003aa997f3fc141c7eacac2b46
xiaotao Nie <onlythen@yeah.net>
2017-07-23 23:52:39 +0800

    update readme

--- config/mysql_config.js
+++ config/mysql_config.js
@@ -2,7 +2,10 @@
  * Created by hh on 12/8/2016.
  */
 var config = {
-
+DBHOST:"mysql.rdsmf0bdg7ny0we.rds.gz.baidubce.com",
+DBUSER:"user1",
+DBPWD:"sinA01234",
+DBNAME:"hdm182127169_db"
 };
 
 module.exports = config;
